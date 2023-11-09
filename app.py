bcb10743e2d1504646d17543e56d97e2678303ac
Bernhard Wallisch <bernhard_wallisch@hotmail.com>
2023-11-09 12:39:29 +0100

    add generic bot handling

--- app.py
+++ app.py
@@ -1,33 +1,81 @@
 #!/bin/python
 import os
+import sys
+import logging
 from datetime import datetime
 from dotenv import load_dotenv
 
 import discord
+from discord.ext import commands
 
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(levelname)s:%(name)s: %(message)s')
+logger = logging.getLogger(__name__) # This logger will inherit the settings from the root logger, configured above
+
+
+# Load configuration
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+DEV_USER_ID = os.getenv('DEV_USER_ID')
 
+
+# Setup discord connection and the bot
 intents = discord.Intents.default()
 intents.message_content = True
+intents.messages = True
+intents.guilds = True
 intents.members = True  # This is necessary to access the member list
-client = discord.Client(intents=intents)
+
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 
-@client.event
+# Discord Event Handlers:
+@bot.event
 async def on_ready():
-    guild = discord.utils.get(client.guilds, name=GUILD)
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
+    guild = discord.utils.get(bot.guilds, name=GUILD)
+    logger.info(
+        f'{bot.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})\n'
     )
 
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
+    members = '\n - '.join([str(member) for member in guild.members])
+    logger.info(f'Guild Members:\n - {members}\n')
+
+    channels = '\n - '.join([str(channel) for channel in guild.text_channels])
+    logger.info(f'Guild Channels:\n - {channels}\n')
+
+    general = guild.text_channels[0]   #discord.utils.get(guild.text_channels, name="Allgemein")  # in english
+    if general and general.permissions_for(guild.me).send_messages:
+        await general.send(
+            f'Hello! I am now online and will moderate the Werewolves games!\n'
+            f'Enter !help to get a list of all commands.'
+            )
+    else:
+        logger.warn(f"No 'general' channel found in {guild.name} or missing permission to send messages.")
 
 
-@client.event
+
+@bot.command(name='quit', help='Shuts down the bot (accessible only by the master-user ;-) )')
+async def quit(ctx):
+    if str(ctx.author.id) == DEV_USER_ID:
+        logger.info(f'{ctx.author.id} shuts down the bot!')
+        await ctx.send("I'll take a nap, bye...")
+        await bot.close()
+        sys.exit(0)
+    else:
+        logger.warn(f'{ctx.author.id} wanted to shut down the bot!')
+        await ctx.send('You do not have permission to shut down the bot.')
+
+
+@bot.command(name='rules', help='Display the game rules')
+async def show_rules(ctx):
+    with open('doc/GAMEPLAY.md','r') as f:
+        await ctx.send(f.read())
+
+
+@bot.event
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(
@@ -35,16 +83,21 @@ async def on_member_join(member):
     )
 
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
     
-    print(f'{message.channel}: {message.author}={message.content}')
+    logger.debug(f'{message.channel}: {message.author}={message.content}')
 
     # write channel messages to later use to analyze and improve the AI-player bot
-    with open(f'data/{message.channel}.txt','a') as f:
+    with open(f'data/{message.channel}.csv','a') as f:
         timestamp_string = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        f.write(f'{timestamp_string} {message.author}: {message.content}\n')
+        f.write(f'{timestamp_string};{message.author};{message.content}\n')
 
-client.run(TOKEN)
+    # Without this, commands won't get processed
+    await bot.process_commands(message)
+
+
+
+bot.run(TOKEN)
---
+++ doc/GAMEPLAY.md
@@ -0,0 +1,30 @@
+# The Werewolves Game
+
+It's a social role-playing game that divides a group of people into two camps: the villagers and the werewolves. The objective of the game differs for the two groups. The werewolves aim to eliminate all the villagers, while the villagers aim to identify and eradicate all the werewolves.
+
+The basic gameplay rules:
+
+**Setup:**
+Each player draws a card that determines their role in the game (e.g., Werewolf, Villager, Seer, Witch, Hunter, etc.).
+Cards are drawn secretly, and each player looks at their card to know their role.
+A game leader is chosen. This person will moderate the game, provide instructions, and help enforce the rules.
+Gameplay:
+The game alternates between night and day phases.
+
+***Night:***
+1. All players close their eyes.
+2. The game leader asks the werewolves to open their eyes and silently select a villager they wish to "kill."
+3. Other special roles, such as the Seer or Witch, may also act, depending on the rules established for those roles.
+4. After all special roles have performed their actions, the day begins.
+
+***Day:***
+1. All players open their eyes.
+2. The game leader announces who has been "killed" during the night.
+3. The villagers discuss who they believe might be a werewolf.
+4. One player is selected by vote to be "killed"; their role is revealed.
+5. A new day-night cycle begins.
+
+**Objective:**
+The villagers win if all werewolves are eliminated.
+The werewolves win if their number is equal to or greater than the remaining villagers.
+There are many variants and additional roles that can be added to the game to make it more interesting or complex.
